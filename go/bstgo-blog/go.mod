aaf6a7e0f971c6ba48dbb161de8c04c8a0fd4519
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-12-21 12:44:16 +0800

    go

---
+++ go/bstgo-blog/go.mod
@@ -0,0 +1,50 @@
+module bstgo-blog
+
+go 1.22
+
+toolchain go1.22.2
+
+require (
+	github.com/BurntSushi/toml v0.4.1
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/segmentio/ksuid v1.0.4
+	go.mongodb.org/mongo-driver v1.7.1
+	go.uber.org/zap v1.19.1
+)
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.7.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/klauspost/compress v1.13.3 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.36.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ go/bstgo-blog/public/admin/edit.js
@@ -0,0 +1,194 @@
+$('#title-edit').keyup(function(e) {
+    let key = e.which
+        //按下回车后抬起
+    if (key == 13) {
+        e.preventDefault()
+    }
+})
+
+$('#title-edit').keydown(function(e) {
+    let key = e.which
+    if (key == 13) {
+        e.preventDefault()
+    }
+})
+
+
+//点击分类下拉菜单
+$('.cat-div button').on('click', function() {
+    //console.log("year button clicked")
+    //设置下拉列表选中状态
+    let text_data = $(this).children('.cat-text').text()
+        //清空之前的选中状态
+    $(this).siblings('ul').children('li').removeClass('active')
+        //设置下拉列表选中状态
+
+    $(this).siblings('ul').children('li').each(function() {
+        if ($(this).children('a').text() == text_data) {
+            $(this).addClass('active')
+        }
+    })
+})
+
+//点击分类列表更新按钮信息
+$('.cat-div ul li').on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active')
+    $(this).parent().siblings('button').children('.cat-text').text($(this).children('a').text())
+    console.log('catid is ', $(this).children('a').attr('catid'))
+    $('.subcat-div>button>.subcat-text').text('不限')
+    if ($(this).children('a').attr('catid') == undefined) {
+        return
+    }
+    //请求子分类
+    $.ajax({
+        type: "POST",
+        url: "/admin/subcatselect",
+        contentType: "application/json",
+        data: JSON.stringify({ 'catid': $(this).children('a').attr('catid') }), //参数列表
+        dataType: "html",
+        success: function(result) {
+            //请求正确之后的操作
+            console.log('post success , result is ', result)
+
+            let index_find = result.indexOf('Sign in')
+                //找到Sign in 说明是登录页面
+            if (index_find != -1) {
+                window.location.href = "/admin/login"
+                return
+            }
+
+            $('.subcat-div>ul').html(result)
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            //请求失败之后的操作
+            console.log('post failed')
+                // 状态码
+            console.log(XMLHttpRequest.status);
+            // 状态
+            console.log(XMLHttpRequest.readyState);
+            // 错误信息   
+            console.log(textStatus);
+        }
+    });
+})
+
+//点击子分类下拉菜单
+$('.subcat-div button').on('click', function() {
+
+    //console.log("year button clicked")
+    //设置下拉列表选中状态
+    let text_data = $(this).children('.subcat-text').text()
+        //清空之前的选中状态
+    $(this).siblings('ul').children('li').removeClass('active')
+        //设置下拉列表选中状态
+
+    $(this).siblings('ul').children('li').each(function() {
+        if ($(this).children('a').text() == text_data) {
+            $(this).addClass('active')
+        }
+    })
+})
+
+//点击月份列表更新按钮信息
+$('.subcat-div ul').on('click', 'li', function() {
+    $(this).addClass('active').siblings().removeClass('active')
+    $(this).parent().siblings('button').children('.subcat-text').text($(this).children('a').text())
+})
+
+//点击发布文章按钮
+$('.publish-edit-btn').on('click', function() {
+
+    let title = $('#title-edit').val()
+    let content = window.maineditor.txt.html()
+    let cat = $('.cat-text').text()
+    let subcat = $('.subcat-text').text()
+    let subtitle = $('.sub-tittle>input').val()
+    let author = $('.author>input').val()
+    console.log('json stringfy is ', JSON.stringify({
+        'title': title,
+        'content': content,
+        'cat': cat,
+        'subcat': subcat,
+        'subtitle': subtitle,
+        'author': author
+    }))
+    $.ajax({
+        type: "POST",
+        url: "/admin/pubarticle",
+        contentType: "application/json",
+        data: JSON.stringify({
+            'title': title,
+            'content': content,
+            'cat': cat,
+            'subcat': subcat,
+            'subtitle': subtitle,
+            'author': author
+        }), //参数列表
+        dataType: "json",
+        success: function(result) {
+            //请求正确之后的操作
+            console.log('post success , result is ', result)
+            if (result.code == 1005) {
+                window.location.href = "/admin/login"
+                return
+            }
+            if (result.code == 0) {
+                window.location.href = "/admin"
+                return
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            //请求失败之后的操作
+            console.log('post failed')
+                // 状态码
+            console.log(XMLHttpRequest.status);
+            // 状态
+            console.log(XMLHttpRequest.readyState);
+            // 错误信息   
+            console.log(textStatus);
+        }
+    })
+})
+
+$(function() {
+    const E = window.wangEditor
+    const editor = new E('#toolbar-container', '#text-container')
+        //    设置编辑区域高度为 500 px
+    editor.config.height = 100
+        //配置菜单
+    editor.config.menus = [
+        'head',
+        'bold',
+        'fontSize',
+        'fontName',
+        'italic',
+        'underline',
+        'strikeThrough',
+        'indent',
+        'lineHeight',
+        'foreColor',
+        'backColor',
+        'link',
+        'list',
+        'todo',
+        'justify',
+        'quote',
+        'emoticon',
+        'image',
+        'video',
+        'table',
+        'code',
+        'splitLine',
+        'undo',
+        'redo',
+    ]
+    hljs.initHighlightingOnLoad(); // 初始化
+    hljs.initLineNumbersOnLoad();
+    editor.highlight = hljs
+    editor.config.languageTab = '    '
+    editor.config.pasteIgnoreImg = false
+    editor.config.uploadImgShowBase64 = true
+    editor.create()
+    window.maineditor = editor
+    console.log('window.editor is ', window.maineditor)
+})

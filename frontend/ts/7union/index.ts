7391de50f014b8d3dfc396ec2af74a9539473b61
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-01-19 20:51:02 +0800

    ts

---
+++ frontend/ts/7union/index.ts
@@ -0,0 +1,43 @@
+// 本章我们会介绍这个联合类型，交叉类型，类型断言
+
+// 联合类型就是对同一个东西支持多种类型
+let phone: number | string = 18737519552
+phone = 'trued'
+
+// 输入为数字或者bool类型都会返回实际的布尔类型
+let fn = (type: number | boolean): boolean => {
+    return !!type
+}
+
+// 交叉类型类似于extends或者重合效果
+interface People {
+    name: string,
+    age: number
+}
+
+interface Man {
+    sex: string
+}
+
+// 交叉类型是一个&，表示把所有的属性结合起来
+const fn2 = (huaixi: Man & People): void => {
+    console.log(huaixi)
+}
+
+fn2({
+    name: 'huaixi',
+    age: 19,
+    sex: 'man'
+})
+
+// 类型断言:即借助as指定类型
+// 此时若是传入string就会调用length方法，若是number就会返回一个undefined
+// 说明这个as只是帮助我们通过这个编译，但是实际的运行时错误无法避免
+let fn3 = (num: number | string): void => {
+    console.log((num as string).length)
+    console.log((<number>num))  // 不推荐
+}
+// 类型断言本质上只是为了通过编译，实际上没有任何转换，下面的例子，输入什么就输出什么
+let fn4 = (type: any): boolean => {
+    return (type as boolean)
+}
---
+++ frontend/ts/8builtObj/index.ts
@@ -0,0 +1,21 @@
+// 本节来介绍一下Ts的内置对象
+
+// ECMAScript内置
+// 类型为对应new出来的类型
+let num: Number = new Number(1)
+let date: Date = new Date()
+let reg: RegExp = new RegExp(/.*?/)
+let error: Error = new Error('error')
+let xhr: XMLHttpRequest = new XMLHttpRequest()
+
+// DOM： 网页内容
+// 类型为HTML(元素名称)Element, 但是若是section这种无特殊标签的类型实际上会修正为HTMLElement
+// 下面有两个例子，一个是非空断言，一个是类型守卫
+let element1: HTMLDivElement = document.querySelector('div')!
+let element2 = document.querySelector('section')
+if (element2) { }
+
+let elements1: NodeList = document.querySelectorAll('div')
+let elements2: NodeListOf<HTMLDivElement | HTMLCanvasElement> = document.querySelectorAll('div canvas')
+
+// BOM

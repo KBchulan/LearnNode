5c7921e97ae93fdc7c940e5ddb3f008f4e68c234
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-01-31 17:32:29 +0800

    vue

--- frontend/vue/vite-demo/src/App.vue
+++ frontend/vue/vite-demo/src/App.vue
@@ -1,35 +1,46 @@
-<script setup lang="ts">
-// 写一些ts代码
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <!-- 写一些标签一类的 -->
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <!-- 1.vue模板：模板可以支持运算一类的，也可以支持API调用 -->
+  <p>
+    {{ sum() ? 'true' : 'false' }}<br>
+    {{ arr1.map(value => ({ num: value })) }}
+  </p>
+
+  <!-- 2.vue指令 -->
+  <span v-text="str1"></span>         <!-- 可以解析文本 -->
+  <strong v-html="str2"></strong>     <!-- 可以解析html标签 -->
+  
+  <!-- 可以解析布尔值，为true显示true，为false会被更改为注释节点 -->
+  <em v-if="bool1">{{ bool1 }}</em>
+  <em v-if="bool2">{{ bool2 }}</em>
+
+  <!-- 同样也是解析布尔值，但与v-if不同的是，v-show在false时会把css的display属性设置为none，并不是注释 -->
+  <div v-show="bool1">{{ bool1 }}</div>  
+  <div v-show="bool2">{{ bool2 }}</div>
+  
 </template>
 
-<style scoped>
-/* 写一些样式 */
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+// 写一些js/ts代码
+
+// module 1
+const a: number = 1
+const b: number = 2
+const sum = () => {
+  return a + b
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
+const arr1: number[] = [1, 2, 3]
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+// module 2
+const str1: string = 'this is a string'
+const str2: string = '<section style="color: green">this is a section</section>'
+const bool1: boolean = true
+const bool2: boolean = false
+
+</script>
+
+<style>
+/* 本位置写一些样式一类的 */
 </style>

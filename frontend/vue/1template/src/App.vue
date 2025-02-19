d5720f2c363092099043199020f585efcd90e3dd
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-02-19 21:48:20 +0800

    vue

--- frontend/vue/1template/src/App.vue
+++ frontend/vue/1template/src/App.vue
@@ -1,55 +1,57 @@
 <template>
   <!-- 写一些标签一类的 -->
-
-  <!-- 1.vue模板：模板可以支持运算一类的，也可以支持API调用 -->
-  <p>
-    {{ sum() ? 'true' : 'false' }}<br>
-    {{ arr1.map(value => ({ num: value })) }}
-  </p>
-
-  <!-- 2.vue指令 -->
-  <span v-text="str1"></span> <!-- 可以解析文本 -->
-  <strong v-html="str2"></strong> <!-- 可以解析html标签 -->
-
-  <!-- v-if可以解析布尔值，为true显示true，为false会被更改为注释节点 -->
-  <em v-if="bool1">{{ bool1 }}</em>
-  <em v-if="bool2">{{ bool2 }}</em>
-
-  <!-- v-show同样也是解析布尔值，但与v-if不同的是，v-show在false时会把css的display属性设置为none，并不是注释 -->
-  <div v-show="bool1">{{ bool1 }}</div>
-  <div v-show="bool2">{{ bool2 }}</div>
-
-  <!-- 那相对应的，肯定也存在v-else, v-else-if, v-else-if-else -->
-  <div v-if="str3 === 'A'">A</div>
-  <div v-else-if="str3 === 'B'">B</div>
-  <div v-else>C</div>
-
-  <!-- 这里说一下v-on，可以绑定事件 -->
-  <button v-on:click="func1">这是一个按钮</button> <!-- 这个是v-on版 -->
-  <button @click="func1">这是一个按钮(@版)</button> <!-- 这个是@版，比较常用 -->
-  <button @[event1]="func1">这是一个按钮(@[]版)</button> <!-- 这个是@[]版,也就是我们的动态事件 -->
-
-  <!-- 这里写一下v-bind -->
-  <div v-bind:id="id">这是一个v-bind演示</div>
-  <div :id="id">这是一个v-bind演示(简写版本)</div>
-  <div :style="style">这是一个v-bind演示(style)</div>
-  <div :class="[bool3 ? 'a' : 'b']">这是一个v-bind演示(class)</div> <!-- 可以同时支持一个动态的，一个静态的 -->
-
-  <!-- 这里写一下v-model,用于实现双向数据绑定,即让数据和视图同步 -->
   <div>
-    <input v-model="name"><br> <!-- 文本框 -->
+    <!-- 1.vue模板：模板可以支持运算一类的，也可以支持API调用 -->
+    <p>
+      {{ sum() ? 'true' : 'false' }}<br>
+      {{arr1.map(value => ({ num: value }))}}
+    </p>
 
-    <textarea v-model="textarea"></textarea><br> <!-- 多行文本 -->
+    <!-- 2.vue指令 -->
+    <span v-text="str1"></span> <!-- 可以解析文本 -->
+    <strong v-html="str2"></strong> <!-- 可以解析html标签 -->
 
-    <input type="checkbox" v-model="checked"><br> <!-- 复选框 -->
+    <!-- v-if可以解析布尔值，为true显示true，为false会被更改为注释节点 -->
+    <em v-if="bool1">{{ bool1 }}</em>
+    <em v-if="bool2">{{ bool2 }}</em>
 
-    <input type="radio" v-model="radio" value="1"> <!-- 单选按钮 -->
-    <input type="radio" v-model="radio" value="2"><br>
+    <!-- v-show同样也是解析布尔值，但与v-if不同的是，v-show在false时会把css的display属性设置为none，并不是注释 -->
+    <div v-show="bool1">{{ bool1 }}</div>
+    <div v-show="bool2">{{ bool2 }}</div>
 
-    <select v-model="selected"> <!-- 选择框 -->
-      <option value="1">选项1</option>
-      <option value="2">选项2</option>
-    </select>
+    <!-- 那相对应的，肯定也存在v-else, v-else-if, v-else-if-else -->
+    <div v-if="str3 === 'A'">A</div>
+    <div v-else-if="str3 === 'B'">B</div>
+    <div v-else>C</div>
+
+    <!-- 这里说一下v-on，可以增加事件 -->
+    <button v-on:click="func1">这是一个按钮</button> <!-- 这个是v-on版 -->
+    <button @click="func1">这是一个按钮(@版)</button> <!-- 这个是@版，比较常用 -->
+    <button @[event1].stop="func1">这是一个按钮(@[]版)</button> <!-- 这个是@[]版,也就是我们的动态事件 -->
+    <!-- v-on需要说一下，这个是会从自己这一层触发父级的同名事件的，我们可以对当前事件多一个.stop来阻止这个冒泡 -->
+
+    <!-- 这里写一下v-bind，可以给元素绑定上一个属性，比如给div容器多上一个id一类的 -->
+    <div v-bind:id="id">这是一个v-bind演示</div>
+    <div :id="id">这是一个v-bind演示(简写版本)</div>
+    <div :style="style">这是一个v-bind演示(style)</div>
+    <div :class="[bool3 ? 'a' : 'b']">这是一个v-bind演示(class)</div> <!-- 可以同时支持一个动态的，一个静态的 -->
+
+    <!-- 这里写一下v-model,用于实现双向数据绑定,即让数据和视图同步 -->
+    <div>
+      <input v-model="name"><br> <!-- 文本框 -->
+
+      <textarea v-model="textarea"></textarea><br> <!-- 多行文本 -->
+
+      <input type="checkbox" v-model="checked"><br> <!-- 复选框 -->
+
+      <input type="radio" v-model="radio" value="1"> <!-- 单选按钮 -->
+      <input type="radio" v-model="radio" value="2"><br>
+
+      <select v-model="selected"> <!-- 选择框 -->
+        <option value="1">选项1</option>
+        <option value="2">选项2</option>
+      </select>
+    </div>
   </div>
 
   <!-- 这里写一下v-for，只是遍历，所有有那种二维啊，对象都是可以的 -->
@@ -130,7 +132,7 @@ let count = ref(0)
   color: blue;
 }
 
-.c{
+.c {
   color: green;
   font-size: 20px;
   background-color: rgba(210, 210, 210, 0.5);
@@ -140,6 +142,6 @@ let count = ref(0)
   margin: 10px;
   text-align: center;
   cursor: pointer;
-  
+
 }
 </style>
--- frontend/vue/review/src/App.vue
+++ frontend/vue/review/src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+let num: number = 1
+const cahnge = () => {
+  num++
+  console.log(num)
+}
 </script>
 
 <template>
-
+  <div>
+    <button v-once @click="cahnge">
+      {{ num }}
+    </button>
+  </div>
 </template>
 
 <style scoped></style>

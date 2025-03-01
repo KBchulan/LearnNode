8a7b4a0177c8c7084f10149abc25816a57ed3ff7
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-01 21:17:28 +0800

    loadImage

--- frontend/vue/24loadimage/src/App.vue
+++ frontend/vue/24loadimage/src/App.vue
@@ -1,30 +1,48 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="images">
+      <img v-lazy="item" v-for="item in arr" width="360" height="500" alt="">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import * as types from './types'
+import type { Directive, DirectiveBinding } from 'vue'
+
+// 这里说一下eager的作用，普通的glob是一个懒加载的，也就是会返回一个回调，需要我们调用才能实现图片的加载，
+// 但是加上eager之后则会被直接改为一个module的模式，类似于直接在代码里import
+let imageList = import.meta.glob('./assets/images/*.jpg', { eager: true })
+let arr = Object.values(imageList).map(va => (va as types.ImageValue).default)    // 这里也可以使用Record约束原对象
+
+// Directive两个泛型，1,当前的HTML元素类型，2.绑定值的类型
+let vLazy: Directive<HTMLImageElement, string> = async (el: HTMLImageElement, bingings: DirectiveBinding<string>) => {
+  const defaultImage = await import('./assets/vue.svg')
+
+  el.src = defaultImage.default
+
+  // 检测图片出现的程度
+  const observer = new IntersectionObserver((entries) => {
+    // 若出现在屏幕内则进行加载，即intersectionRatio > 0
+    if (entries[0].intersectionRatio) {
+      // 但是加载的效果比较快，不好看出来，我们加一个Timer
+      setTimeout(() => {
+        el.src = bingings.value
+        observer.unobserve(el)
+      }, 1000)
+
+    }
+  })
+
+  observer.observe(el)
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+</script>
+
+<style lang="less">
+.images {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ frontend/vue/24loadimage/src/types.ts
@@ -0,0 +1,5 @@
+export interface ImageValue {
+  default: string
+  get: Function
+  set: Function
+}

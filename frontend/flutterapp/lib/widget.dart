1700fd3dc3d475e26138a4cbcecbd4226d310347
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-14 18:09:27 +0800

    change

--- frontend/flutterapp/lib/widget.dart
+++ frontend/flutterapp/lib/widget.dart
@@ -1,23 +1,10 @@
 // https://docs.flutter.dev/ui
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Row(
-        children: [
-          Expanded(child: BorderImage()),
-          Expanded(child: BorderImage()),
-          Expanded(child: BorderImage())
-        ],
-      ),
-    ));
-  }
+class MyAppState extends ChangeNotifier {
+  var num = 10;
 }
 
 class BorderImage extends StatelessWidget {
@@ -26,13 +13,66 @@ class BorderImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image(
-      image: AssetImage('lib/assets/cursor.jpg'),
-      width: 450,
-      height: 450,
-    );
+        image: AssetImage('lib/assets/cursor.jpg'), width: 200, height: 200);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyAppState(),
+        child: MaterialApp(home: MyHomePage()));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Dash1'),
+          BorderImage(),
+        ]),
+
+        Divider(),
+
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Dash2'),
+          BorderImage(),
+        ]),
+
+        Divider(),
+
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Dash3'),
+          BorderImage(),
+        ]),
+
+        Divider(),
+
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Dash4'),
+          BorderImage(),
+        ]),
+
+        Divider(),
+
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Dash5'),
+          BorderImage(),
+        ]),
+      ],
+    ));
   }
 }
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }

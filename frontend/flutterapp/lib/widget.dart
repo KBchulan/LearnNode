a8a952488fa9ee759301a7725bf2c5430ca8a3ff
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-14 12:58:07 +0800

    flutter

--- frontend/flutterapp/lib/widget.dart
+++ frontend/flutterapp/lib/widget.dart
@@ -8,39 +8,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [BorderImage(), const Text('Dash 1')]),
-            SizedBox(width: 20),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [BorderImage(), const Text('Dash 2')]),
-            SizedBox(width: 20),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [BorderImage(), const Text('Dash 3')]),
-          ],
-        ),
+        home: Scaffold(
+      body: Row(
+        children: [
+          Expanded(child: BorderImage()),
+          Expanded(child: BorderImage()),
+          Expanded(child: BorderImage())
+        ],
       ),
-    );
+    ));
   }
 }
 
 class BorderImage extends StatelessWidget {
-  const BorderImage({
-    super.key,
-  });
+  const BorderImage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Image(
       image: AssetImage('lib/assets/cursor.jpg'),
-      width: 200,
-      height: 200,
+      width: 450,
+      height: 450,
     );
   }
 }

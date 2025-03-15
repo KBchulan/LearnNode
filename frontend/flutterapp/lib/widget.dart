85263329feb65670b9495b46845140b62a7923bb
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-15 21:09:22 +0800

    goto state

--- frontend/flutterapp/lib/widget.dart
+++ frontend/flutterapp/lib/widget.dart
@@ -4,17 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MyAppState extends ChangeNotifier {
-  var num = 10;
-}
-
-class BorderImage extends StatelessWidget {
-  const BorderImage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Image(
-        image: AssetImage('lib/assets/cursor.jpg'), width: 200, height: 200);
-  }
+  var strs = <String>[
+    '我是字符串1',
+    '我是字符串2',
+    '我是字符串3',
+    '我是字符串4',
+    '我是字符串5',
+    '我是字符串6',
+    '我是字符串7',
+  ];
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-        create: (context) => MyAppState(),
+        create: (context) => MyAppState(), // create a global context
+
         child: MaterialApp(home: MyHomePage()));
   }
 }
@@ -33,43 +32,16 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var strs = context.watch<MyAppState>().strs;
+
     return Scaffold(
-        body: ListView(
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text('Dash1'),
-          BorderImage(),
-        ]),
-
-        Divider(),
-
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text('Dash2'),
-          BorderImage(),
-        ]),
-
-        Divider(),
-
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text('Dash3'),
-          BorderImage(),
-        ]),
-
-        Divider(),
-
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text('Dash4'),
-          BorderImage(),
-        ]),
-
-        Divider(),
-
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text('Dash5'),
-          BorderImage(),
-        ]),
-      ],
-    ));
+        body: ListView.builder(
+      itemCount: strs.length,
+      itemBuilder: (context, index) {
+        return Text(strs[index]);
+      },
+    )
+    );
   }
 }
 
